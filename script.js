854cb6521297892fd40f812b4f7058c030d16a51
Allan <112294367+AllanC12@users.noreply.github.com>
2022-12-02 21:25:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,79 @@
- const form = document.getElementById('container-form')
+   const form = document.getElementById('container-form')
 
  form.addEventListener('submit',(e)=>{
-    e.preventDefault();
+      e.preventDefault();
 
-    const input_letters = document.getElementById('letters')
-    const input_upperLetters = document.getElementById('upper-letters')
-    const input_numbers = document.getElementById('numbers')
-    const input_simbols = document.getElementById('simbols')
-    const passwordElement = document.getElementById('password')
-    const length = parseInt(document.getElementById('quantity').value);
+      const input_letters = document.getElementById('letters')
+      const input_upperLetters = document.getElementById('upper-letters')
+      const input_numbers = document.getElementById('numbers')
+      const input_simbols = document.getElementById('simbols')
+      const passwordElement = document.getElementById('password')
+      const sizePassword = document.getElementById('quantity').value;
+      const int_size_password = parseInt(document.getElementById('quantity').value);
 
-    const char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    const char_upper = ['Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A']
-    const numbers = [0,1,2,3,4,5,6,7,8,9];
-    const simbol = ['#','$','%','&','*','+']
+      const char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+      const char_upper = ['Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A']
+      const numbers = [0,1,2,3,4,5,6,7,8,9];
+      const simbol = ['#','$','%','&','*','+']
 
-    let password = ''
-   
-    let checkSelected = 0;
-    const checkbox = document.querySelectorAll('.check')
-    //contando quantos checkbox estão selecionados para fazer a divisão pelo tamanho da senha no laço FOR
-    function countCheck(){
-       checkbox.forEach(check => {
-          if(check.checked){
-             checkSelected++;
-          } 
-       });
-    }countCheck()
-
-    //o resultado da divisão entre o valor de input number e os checkbox selecionados 
-    //será o numero de vezes que o laço FOR deverá ser executado
-
-    for(i = 0; i < length / checkSelected; i++){
-      let randChar = Math.floor(Math.random() * char.length)
-      let randSimbol = Math.floor(Math.random() * simbol.length)
-      let randnumber = Math.floor(Math.random() * numbers.length)
-
-       if(input_upperLetters.checked){
-          password += char_upper[randChar];
-          
-       }
-
-       if(input_letters.checked){
-         password += char[randChar]
+      let password = ''
+      
+      let checkSelected = 0;
+      const checkbox = document.querySelectorAll('.check')
+      //contando quantos checkbox estão selecionados para fazer a divisão pelo tamanho da senha no laço FOR
+         function countCheck(){
+            checkbox.forEach(check => {
+               if(check.checked){
+                  checkSelected++;
+               } 
+            });
+         }countCheck()
+  
+     //Impedindo que o usuário crie uma senha sem definir caracteres
+         if(checkSelected == 0){
+            alert('Defina os caracteres da senha!')
+            passwordElement.innerText = '';
+            return;
+         }
+     //Impedindo que o usuário crie uma senha sem definir um tamanho
+         if(sizePassword == ""){
+            alert('Defina um tamanho para a senha')
+            passwordElement.innerText = ''
+            return;
          }
 
-       if(input_numbers.checked){
-          password += numbers[randnumber];
-       }
 
-       if(input_simbols.checked){
-          password += simbol[randSimbol];
-       }
-     }  
+         //o resultado da divisão entre o valor de input number e os checkbox selecionados 
+         //será o numero de vezes que o laço FOR deverá ser executado
 
-   // limitando a senha ao tamanho do valor definido no input type number
-    if(length % checkSelected != 0){
-      password = password.slice(0,length)
-     }
+         for(i = 0; i < int_size_password / checkSelected; i++){
+            let randChar = Math.floor(Math.random() * char.length)
+            let randSimbol = Math.floor(Math.random() * simbol.length)
+            let randnumber = Math.floor(Math.random() * numbers.length)
 
-       passwordElement.innerText = password
+            if(input_upperLetters.checked){
+               password += char_upper[randChar];
+               
+            }
+
+            if(input_letters.checked){
+               password += char[randChar]
+               }
+
+            if(input_numbers.checked){
+               password += numbers[randnumber];
+            }
+
+            if(input_simbols.checked){
+               password += simbol[randSimbol];
+            }
+         }  
+
+         // limitando a senha ao tamanho do valor definido no input type number
+         if(int_size_password % checkSelected != 0){
+            password = password.slice(0,int_size_password)
+         }
+
+            passwordElement.innerText = password
  
    })
